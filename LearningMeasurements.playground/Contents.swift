12342f5036ae258bff974194a5679d83fb57a47e
Rommel Rico <me@rommelrico.com>
2017-10-29 15:07:47 -0700

    Manipulating Measurements

--- LearningMeasurements.playground/Contents.swift
+++ LearningMeasurements.playground/Contents.swift
@@ -5,3 +5,12 @@ import Foundation
 // 1. Creating a measurement.
 let milk = Measurement(value: 1, unit: UnitVolume.imperialPints)
 milk.converted(to: .liters)
+
+// 2. Manipulating measurements.
+let fivePints = milk * 5
+fivePints.converted(to: .cups)
+let kms = Measurement(value: 5, unit: UnitLength.kilometers)
+let meters = Measurement(value: 5000, unit: UnitLength.meters)
+kms == meters
+kms + meters
+
